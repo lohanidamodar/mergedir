60918041da6b6c681ca1288b121e779b129e5c18
lohanidamodar <lubbird@gmail.com>
2015-09-11 14:28:39 +0545

    updated log message

--- mergedir/mergedir.py
+++ mergedir/mergedir.py
@@ -25,10 +25,10 @@ class Mergedir:
         # the merging code
         # exit if source does not exist
         if not path.exists(sourcedir) or not path.isdir(sourcedir):
-            print("Error the source directory does not exist")
+            print("error! invalid source directory")
             return
         if not path.exists(destdir) or not path.isdir(destdir):
-            print("Error the destination directory does not exist")
+            print("error! invalid destination directory")
             return
         for fpath, subdirs, files in os.walk(sourcedir):
             for filename in files:
@@ -40,17 +40,18 @@ class Mergedir:
         filepath, filename = path.split(sourcefile)
         destfile = path.join(destdir,filename)
         if path.exists(destfile):
-            print(filename+" file already exists, replacing giving a new name")
+            print(filename+" file already exists in the destination directory")
             if not filecmp.cmp(sourcefile,destfile):
                 # get a new name for destfile
+                print("files and not same, giving a new name")
                 self.newnamecount += 1
                 name, extension = path.splitext(filename)
                 newfilename = name + "_" + str(self.newnamecount) + extension
                 destfile = path.join(destdir,newfilename)
-                print("Both files use same name")
+
             else:
                 #do nothing
-                print("Both are same file skipping")
+                print("files are same, skipping copy")
                 return
         shutil.copy(sourcefile, destfile)
         return
